e24923c7f1b4be5a5edc24da7a04a4254918a3c8
rksachin <rk.sachin@gmail.com>
2019-12-10 05:58:27 +0000

    added cosmos db
    added cosmos db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : '<paste your Azure Cosmos DB connection string here',
+    remoteUrl : 'mongodb://nodejscosmosdb:l0evBk4IHUcUnek5b5Cghb3Mro5llQdlM8s79fyr1tV1rcHglRHigC0krxfLnW5fvKAbKjUcypmRTEuPg6j3QA==@nodejscosmosdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
     
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
